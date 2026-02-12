a9b117001e7de0d851d85e4d95cb83891f47a0cd
Tom# <tom7as6@gmail.com>
2026-02-12 12:49:48 +0100

    Prvý upload môjho projektu

--- main.py
+++ main.py
@@ -1,43 +1,50 @@
+import time
+import requests
+import numpy as np
 from drivers import redpitaya_scpi as scpi
 
+# --- KONFIGURÁCIA ---
 IP = "169.254.4.198"
+TOKEN = "-0NGryDI1atHgmjh3dNMBWrX8KfDczg1GX7p8TGyvHnGt0w8p_wgVHqT6kTLvafOLrTF5-_PV46SzlSldp5Fw=="
+INFLUX_URL = "http://localhost:8086/api/v2/write?org=MojeLab&bucket=Bc.%20praca&precision=ns"
+HEADERS = {"Authorization": f"Token {TOKEN}"}
+
 rp = scpi.scpi(IP)
 
-# reset
-rp.tx_txt("ACQ:RST")
+def capture_to_influx(seconds=60):
+    rp.tx_txt("ACQ:RST")
+    rp.tx_txt("ACQ:DEC 64")
+    
+    fs = 125e6 / 64
+    dt_ns = int((1/fs) * 1e9)
+    
+    start_loop = time.time()
+    print("Zber beží... Sleduj InfluxDB.")
 
-# časová základňa – meníš podľa frekvencie
-rp.tx_txt("ACQ:DEC 64")   # väčšie číslo = pomalšie, viac detailu
+    while (time.time() - start_loop) < seconds:
+        rp.tx_txt("ACQ:START")
+        rp.tx_txt("ACQ:TRIG NOW")
+        
+        # Čakanie na naplnenie buffera (cca 8ms pri DEC 64)
+        time.sleep(0.01) 
+        
+        rp.tx_txt("ACQ:SOUR1:DATA?")
+        raw = rp.rx_txt().strip("{}\n\r ")
+        
+        if not raw: continue
+        
+        samples = raw.split(",")
+        t_start = time.time_ns()
+        
+        # Tvorba Line Protocolu
+        lines = [f"osciloskop,chan=ch1 volt={v} {t_start + (i * dt_ns)}" 
+                 for i, v in enumerate(samples) if v]
+        
+        # Odoslanie do Influxu
+        res = requests.post(INFLUX_URL, data="\n".join(lines), headers=HEADERS)
+        if res.status_code != 204:
+            print(f"Chyba: {res.status_code}, {res.text}")
 
-# trigger
-rp.tx_txt("ACQ:TRIG:LEV 0")
-rp.tx_txt("ACQ:TRIG CH1_PE")
+    print("Zber dokončený.")
 
-rp.tx_txt("ACQ:START")
-rp.tx_txt("ACQ:TRIG NOW")
-
-# čakaj trigger
-while True:
-    rp.tx_txt("ACQ:TRIG:STAT?")
-    if rp.rx_txt().strip() == "TD":
-        break
-
-# načítaj dáta
-rp.tx_txt("ACQ:SOUR1:DATA?")
-data_str = rp.rx_txt().strip("{}\n\r ")
-samples = [float(x) for x in data_str.split(",") if x]
-
-print("Samples:", len(samples))
-
-period = []
-start = None
-
-for i in range(1, len(samples)):
-    if samples[i-1] < 0 and samples[i] >= 0:
-        if start is None:
-            start = i
-        else:
-            period = samples[start:i]
-            break
-
-print ("One period samples:", len(period))
+capture_to_influx(60)
